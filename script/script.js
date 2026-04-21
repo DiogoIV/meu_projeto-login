9407863a8d3b63334582940ca8af7f2e79700dd0
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-04-21 18:58:27 -0300

    att

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
-const inputs = document.querySelectorAll("section#conteiner input");
+const inputs = document.querySelectorAll("section.conteiner input");
+const conteiner = document.querySelector('.conteiner')
+const loginLink = document.querySelector('#btn_login')
+const registerLink = document.querySelector('#btn_register')
+
+registerLink.addEventListener('click', ()=> {
+    conteiner.classList.add('active')
+})
+loginLink.addEventListener('click', ()=> {
+    conteiner.classList.remove('active')
+})
+
+
+
 
 inputs.forEach(input => {
     input.addEventListener("invalid", (e)=>{
