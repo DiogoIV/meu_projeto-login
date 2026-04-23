a1aa10348c4ea26afadbdd6bb90a4d9a5c31974f
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-04-23 19:57:09 -0300

    validando regex

--- script/script.js
+++ script/script.js
@@ -1,26 +1,64 @@
-const inputs = document.querySelectorAll("section.conteiner input");
-const conteiner = document.querySelector('.conteiner')
-const Linkregister = document.querySelector('#btn_register')
-const Linklogin = document.querySelector('#btn_login')
+const el = { 
+    conteiner : document.querySelector('.conteiner'),
+    Linkregister : document.querySelector('#btn_register'),
+    Linklogin : document.querySelector('#btn_login'),
 
+    input_usuarioRegistro: document.querySelector('.input-register #usu'),
+    input_emailRegistro: document.querySelector('.input-register #email'),
+    input_senhaRegistro: document.querySelector('.input-register #senha')
+}
 
+const regexs = {
+    regx_usuario: /^[a-zA-Z](?!.*[._]{2})[\w.]{2,14}[a-zA-Z]$/,
+    regx_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    regx_senha: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9])\S{6,}$/
+}
 
-Linkregister.addEventListener('click', ()=> {
-    conteiner.classList.add('active')
-})
-Linklogin.addEventListener('click', ()=> {
-    conteiner.classList.remove('active')
-})
+function alterarTela() {
+    el.Linkregister.addEventListener('click', ()=> {
+        el.conteiner.classList.add('active')
+    })
 
+    el.Linklogin.addEventListener('click', ()=> {
+        el.conteiner.classList.remove('active')
+    })
+}
 
+function validarformulario(input, regex) {
+    const valor = input.value
 
+    if(regex.test(valor)) {
+        input.style.border = "2px solid green"
+    } else {
+        input.style.border = "2px solid red"
+    }
+}
 
-inputs.forEach(input => {
-    input.addEventListener("invalid", (e)=>{
-        e.preventDefault();
-        input.style.border = "2px solid red";
+el.input_usuarioRegistro.addEventListener('input', ()=> 
+    validarformulario(el.input_usuarioRegistro, regexs.regx_usuario)
+)
+
+el.input_emailRegistro.addEventListener('input', ()=> 
+    validarformulario(el.input_emailRegistro, regexs.regx_email)
+)
+
+el.input_senhaRegistro.addEventListener('input', ()=> 
+    validarformulario(el.input_senhaRegistro, regexs.regx_senha)
+)
+
+function avisodoformulario() {
+    el.inputs.forEach(input => {
+        input.addEventListener("invalid", (e)=>{
+            e.preventDefault();
+            input.style.border = "2px solid red";
+        });
+        input.addEventListener("input", ()=>{
+        input.style.border = "";
+        });
     });
-    input.addEventListener("input", ()=>{
-    input.style.border = "";
-});
-});
+}
+
+
+alterarTela()
+avisodoformulario()
+
