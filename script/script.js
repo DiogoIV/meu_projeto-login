b17bb10f30c01061bc6e96266f6683f18cfec894
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-04-25 10:05:14 -0300

    fazendo ligacao com beck end

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
-const el = { 
-    conteiner : document.querySelector('.conteiner'),
-    Linkregister : document.querySelector('#btn_register'),
-    Linklogin : document.querySelector('#btn_login'),
+const el = {
+    conteiner: document.querySelector('.conteiner'),
+    Linkregister: document.querySelector('#btn_register'),
+    Linklogin: document.querySelector('#btn_login'),
 
     input_usuarioRegistro: document.querySelector('.input-register #usu'),
     input_emailRegistro: document.querySelector('.input-register #email'),
-    input_senhaRegistro: document.querySelector('.input-register #senha')
+    input_senhaRegistro: document.querySelector('.input-register #senha'),
+    button_register: document.querySelector('.register')
 }
 
 const regexs = {
@@ -15,11 +16,11 @@ const regexs = {
 }
 
 function alterarTela() {
-    el.Linkregister.addEventListener('click', ()=> {
+    el.Linkregister.addEventListener('click', () => {
         el.conteiner.classList.add('active')
     })
 
-    el.Linklogin.addEventListener('click', ()=> {
+    el.Linklogin.addEventListener('click', () => {
         el.conteiner.classList.remove('active')
     })
 }
@@ -27,36 +28,68 @@ function alterarTela() {
 function validarformulario(input, regex) {
     const valor = input.value
 
-    if(regex.test(valor)) {
+    if (regex.test(valor)) {
         input.style.border = "2px solid green"
     } else {
         input.style.border = "2px solid red"
     }
 }
 
-el.input_usuarioRegistro.addEventListener('input', ()=> 
+el.input_usuarioRegistro.addEventListener('input', () =>
     validarformulario(el.input_usuarioRegistro, regexs.regx_usuario)
 )
 
-el.input_emailRegistro.addEventListener('input', ()=> 
+el.input_emailRegistro.addEventListener('input', () =>
     validarformulario(el.input_emailRegistro, regexs.regx_email)
 )
 
-el.input_senhaRegistro.addEventListener('input', ()=> 
+el.input_senhaRegistro.addEventListener('input', () =>
     validarformulario(el.input_senhaRegistro, regexs.regx_senha)
 )
 
 function avisodoformulario() {
     el.inputs.forEach(input => {
-        input.addEventListener("invalid", (e)=>{
+        input.addEventListener("invalid", (e) => {
             e.preventDefault();
             input.style.border = "2px solid red";
         });
-        input.addEventListener("input", ()=>{
-        input.style.border = "";
+        input.addEventListener("input", () => {
+            input.style.border = "";
         });
     });
 }
+
+
+el.button_register.addEventListener('click', async (ele) => {
+    ele.preventDefault()
+
+    const usuariok = el.input_usuarioRegistro.addEventListener('input', () =>
+        validarformulario(el.input_usuarioRegistro, regexs.regx_usuario)
+    )
+    const emailok = el.input_emailRegistro.addEventListener('input', () =>
+        validarformulario(el.input_emailRegistro, regexs.regx_email)
+    )
+    const senhaok = el.input_senhaRegistro.addEventListener('input', () =>
+        validarformulario(el.input_senhaRegistro, regexs.regx_senha)
+    )
+
+    if(!usuariok||!emailok|| !senhaok) {
+        return alert('preencha os dados corretamente')
+    } 
+
+    const res = awaitfetch('http:://localhost:3000/usuario', {
+        method:'POST',
+        Headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            usuario: el.input_usuarioRegistro.value,
+            email: el.input_emailRegistro.value,
+            senha: el.input_senhaRegistro.value
+             
+        })
+    })
+})
 
 
 alterarTela()
