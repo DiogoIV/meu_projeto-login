47d903e3ae50a66e979c9ad6d0ccb9cbb27df68d
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-06-03 21:07:24 -0300

    aaddddddd

--- script/servidor.js
+++ script/servidor.js
@@ -7,15 +7,17 @@ dotenv.config()
 const app = express()
 
 // 1. CORS primeiro (antes das rotas)
-app.use(cors({
-  origin: 'https://meu-projeto-login-dagm7tkix-diogoivs-projects.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-  
-}))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
-// 2. Preflight OPTIONS
-app.options('*', cors())
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
 
 // 3. JSON parser
 app.use(express.json())
